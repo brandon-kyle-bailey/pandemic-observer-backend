c36efbec1bd375b880746118d01b2c7078553fb8
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-04-16 19:25:56 -0300

    creating initial global api route

---
+++ app/controllers/summary.controller.js
@@ -0,0 +1,41 @@
+const https = require('https');
+
+
+exports.global = (request, response) => {
+
+    var response_body;
+
+    https.get('https://api.covid19api.com/summary', (res) => {
+
+        var body = '';
+
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+            body += d;
+        });
+
+        res.on('end', () => {
+            response_body = JSON.parse(body);
+            response.json({
+                "message":"this is managed by the summary controller.",
+                "data": {
+                    "global": response_body["Global"],
+                },
+            });
+        });
+
+    }).on('error', (e) => {
+
+        console.error(e);
+
+    });
+
+    //response.json({
+    //    "message":"this is managed by the summary controller.",
+    //    "data": {
+    //        "global": response_body.global,
+    //    },
+    //});
+};
