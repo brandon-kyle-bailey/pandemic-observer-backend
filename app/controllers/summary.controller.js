bc4c3a51a93ac4428d13b865c33dc0a82d87a5ac
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-04-19 19:57:50 -0300

    [patch] testing production timeout of all route

--- app/controllers/summary.controller.js
+++ app/controllers/summary.controller.js
@@ -20,6 +20,25 @@ makeHttpRequest = function(uri, callbackFunc) {
 
 exports.all = (req, res) => {
     makeHttpRequest(process.env.API_ROOT, (data) => {
-        res.json(data)
+        res.json({"data": data})
     });
 };
+
+
+exports.test = (req, res) => {
+
+     https.get('https://api.covid19api.com/summary', (response) => {
+         console.log('statusCode:', response.statusCode);
+         console.log('headers:', response.headers);
+         var body = [];
+         response.on('data', (data) => {
+             body.push(data);
+         });
+         response.on('end', () => {
+             var test_data = JSON.parse(body.join(''));
+             res.json({"data": test_data["Global"]})
+         });
+     }).on('error', (err) => {
+         console.log(err);
+     });
+}
--- app/routes/summary.routes.js
+++ app/routes/summary.routes.js
@@ -5,5 +5,7 @@ const summary = require('../controllers/summary.controller.js');
 
 router.get('/all', summary.all);
 
+router.get('/test', summary.test);
+
 
 module.exports = router;
